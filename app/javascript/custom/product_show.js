df51beab17efbf32fb3b2a34438e5ad03674f816
musa1992 <mosesbwire91@gmail.com>
2021-10-03 22:14:13 +0300

    add infinite scroll product view carousel on product show page

--- app/javascript/custom/product_show.js
+++ app/javascript/custom/product_show.js
@@ -2,26 +2,56 @@ document.addEventListener("turbolinks:load",()=>{
     if (!Turbolinks) {
         location.reload();
     }
-    var arrow = document.getElementsByClassName('arrow');
-    var productThumbnail = document.getElementsByClassName('product-thumbnail');
+    const next = document.querySelector('.next');
+    const prev = document.querySelector('.prev');
+    const slider = document.querySelector('.slider')
+    var clickedElem
+    var direction;
+
+    next.addEventListener('click',()=>{
+        direction = -1
+        slider.style.transform= "translateY(-112px)"  
+         
+    })
+    prev.addEventListener('click',()=>{
+       
+        direction = 1
+        slider.style.transform = "translateY(112px)"
+    })
+
+    slider.addEventListener('click', (e)=>{
+        direction = 0
+        let px = e.target.offsetTop
+        clickedElem = e.target
+        slider.style.transform = "translateY(-" + px + "px)"
+    })
 
 
-    var l = 0;
-    arrow[0].onclick = ()=> {
-        l ++;
-        console.log(productThumbnail.length)
-        for (let i = 0; i < productThumbnail.length; i ++){
-            if (l == 0 ){
-                productThumbnail[i].style.top = "0px"
-            }
-            if (l ==1){
-                productThumbnail[i].style.top = "-126px"
-            }
-            if (l == 2){
-                productThumbnail[i].style.top = "-247px"
-            }
-        }
-    }
+    slider.addEventListener('transitionend', function(){
     
+        if (direction == -1){
+           slider.append(slider.firstElementChild) 
+        }else if (direction == 1){
+            slider.prepend(slider.lastElementChild)
+        }else if (direction == 0 ){
+            do {
+                slider.appendChild(slider.firstElementChild) 
+            }while (clickedElem.previousElementSibling != clickedElem.parentNode.firstElementChild){
+                slider.appendChild(slider.firstElementChild)
+            }   
+        }
+        
+        slider.style.transition= "none"
+        slider.style.transform = "translateY(0)"
+        setTimeout(function(){
+            slider.style.transition = "all 0.5s"
+        })   
+    })
+
+    
+
+
+    
+   
 })
 
