19fe84344480319958be35dac953c43d31f7610e
musa1992 <mosesbwire91@gmail.com>
2021-10-15 05:50:34 +0300

    finish show product show page

--- app/javascript/custom/product_show.js
+++ app/javascript/custom/product_show.js
@@ -7,21 +7,30 @@ document.addEventListener("turbolinks:load",()=>{
     const slider = document.querySelector('.slider')
     const productSlider = document.querySelector('.product-slider')
     const imageWidth = document.getElementsByClassName('product-image')[0].clientWidth
+    const productSize = document.querySelector('.product-size');
+    const size = document.getElementById('size')
+    const availableSizes  = ["Small", "Medium", "Large","X Large", "XX Large"]
+    const sizeMeasurement = productSize.children
+   const quantitySelector = document.querySelector('.quantity-selector')
+   const quantity = document.querySelector('.quantity')
     var clickedElem
     var direction;
+    var clickedMeasurement
+
     
+
     next.addEventListener('click',()=>{
         
         direction = -1
     
-        slider.style.transform = "translateY(-106px)"
+        slider.style.transform = "translateY(-126px)"
         productSlider.style.transform = "translateX(-" + imageWidth + "px)"
          
     })
     prev.addEventListener('click',()=>{
        
         direction = 1
-        slider.style.transform = "translateY(112px)"
+        slider.style.transform = "translateY(132px)"
         productSlider.style.transform = "translateX(" + imageWidth + "px)"
 
 
@@ -30,7 +39,7 @@ document.addEventListener("turbolinks:load",()=>{
     slider.addEventListener('click', (e)=>{
         direction = 0
         let px = e.target.offsetTop
-        let productSliderFactor = (px/106) * imageWidth
+        let productSliderFactor = (px/126) * imageWidth
         clickedElem = e.target
         slider.style.transform = "translateY(-" + px + "px)"
         productSlider.style.transform = "translateX(-" + productSliderFactor + "px)"
@@ -80,6 +89,43 @@ document.addEventListener("turbolinks:load",()=>{
             productSlider.style.transition = "all 0.5s"
         })   
     })
+
+    productSize.addEventListener('click', (e)=> {
+       let el = e.target
+       let index = Array.from(el.parentElement.children).indexOf(el)
+       size.innerHTML = availableSizes[index]
+        if (typeof clickedMeasurement !== 'undefined'){
+            clickedMeasurement.style.borderBottom = "none"
+        }
+        e.target.style.borderBottom = "3.5px #000 solid"
+
+        clickedMeasurement = e.target
+    })
+
+    sizeMeasurement[0].click()
+
+    quantitySelector.addEventListener('click', (e)=> {
+        let qty = parseInt(quantity.innerHTML)
+
+        if (e.target.classList.contains('qty-add') || e.target.classList.contains('fa-plus')){
+            qty += 1
+            quantity.innerHTML = qty
+        }
+
+        if (e.target.classList.contains('qty-minus') || e.target.classList.contains('fa-minus')){
+            qty -= 1
+            if (qty == 0){
+                quantity.innerHTML = "1"
+            }else {
+                quantity.innerHTML = qty
+            }
+            
+        }
+
+        
+
+    })
+
   
 })
 
